046737160d0b3a931ddde6f828607b4e8ab29fb8
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2020-08-25 16:38:10 +0430

    Little change

--- app/src/main/java/com/navigation/singleactivity/SampleDataModel.kt
+++ app/src/main/java/com/navigation/singleactivity/SampleDataModel.kt
@@ -1,7 +1,9 @@
 package com.navigation.singleactivity
 
+import androidx.annotation.Keep
 import java.io.Serializable
 
+@Keep
 data class SampleDataModel(
     val name: String,
     val family: String
